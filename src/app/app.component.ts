bbaf6ee11a2eaa24a3db14a583bd555b2b5274cf
kristenjin811 <97808382+kristenjin811@users.noreply.github.com>
2023-07-19 10:07:35 -0700

    import data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Person } from './person.model';
+import * as data from './people.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  people: Person[] = (data as any).default; // This will contain the array of people
+
+  deletePerson(id: string) {
+    this.people = this.people.filter(person => person.id !== id);
+  }
 }
-
-
---
+++ src/app/person.model.ts
@@ -0,0 +1,7 @@
+export class Person {
+  constructor(
+    public id: string,
+    public firstName: string,
+    public lastName: string,
+  ) { }
+}
